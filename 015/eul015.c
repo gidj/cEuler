ff022f7434072b19994c3e804b2b677a40e62e2c
Gideon VanRiette <gvanriette@gmail.com>
2014-01-14 20:26:01 +0900

    Finished: had to implement a Dynamic Programming algorithm to calculate it, as using the binomial coefficient direct calculation exceeds the limits of even long long unsigned types in C

--- 015/eul015.c
+++ 015/eul015.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <limits.h>
 
 long long unsigned n_choose_r(long, long);
 long long unsigned new_choose_r(long, long);
 long long unsigned factorial(long);
+long long unsigned lattice_value(long long unsigned a[][40], long, long);
 
 
 int main(int argc, char *argv[])
@@ -17,10 +19,26 @@ int main(int argc, char *argv[])
    * by x choose y, where x is the row and y is the index of the element
    * in that row. */
 
+  
+  long long unsigned array[40][40] = {0};
+
+  printf("Let's try 2, 2 (should be 6): %llu\n", lattice_value(array, 2, 2));
+  printf("Let's try 3, 3 (should be 20): %llu\n", lattice_value(array, 3, 3));
+  printf("Let's try 3, 4 (should be 35): %llu\n", lattice_value(array, 3, 4));
+  printf("Let's try 20, 20 (should be the answer): %llu\n", lattice_value(array, 20, 20));
+
+
+
+
+
+
+
   for (int i = 0; i < 30; ++i)
   {
     printf("%llu\n", factorial(i));
   }
+
+  printf("long long unsiged max: %llu\n", ULLONG_MAX);
 
   printf("5 choose 3: %llu\n", n_choose_r(5, 3));
   printf("5 choose 3, new: %llu\n", new_choose_r(5, 3));
@@ -30,6 +48,26 @@ int main(int argc, char *argv[])
   printf("15 choose 3, new: %llu\n", new_choose_r(15, 3));
   printf("This is the nunber of paths: %llu\n", new_choose_r(38, 19));
   return 0;
+}
+
+long long unsigned lattice_value(long long unsigned a[][40], long x, long y)
+{
+  if (a[x][y] > 0)
+  {
+    return a[x][y];
+  }
+  else if (x == 0 || y == 0)
+  {
+    a[x][y] = 1;
+    a[y][x] = 1;
+    return 1;
+  }
+  else
+  {
+    a[x][y] = lattice_value(a, x, y-1) + lattice_value(a, x-1, y);
+    a[y][x] = a[x][y];
+    return a[x][y];
+  }
 }
 
 long long unsigned factorial(long n)
